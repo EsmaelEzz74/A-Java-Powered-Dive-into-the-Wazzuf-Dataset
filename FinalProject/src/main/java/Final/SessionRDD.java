bb1fada0cef052aac88b419f4d47b3dcf53f80e8
EsmaelEzz74 <85246622+EsmaelEzz74@users.noreply.github.com>
2021-07-02 21:27:55 +0200

    Update SessionRDD.java

--- FinalProject/src/main/java/Final/SessionRDD.java
+++ FinalProject/src/main/java/Final/SessionRDD.java
@@ -2,19 +2,15 @@ package Final;
 
 import org.apache.spark.sql.DataFrameReader;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.ml.feature.VectorAssembler;
-import org.apache.spark.ml.regression.LinearRegression;
-import org.apache.spark.ml.regression.LinearRegressionModel;
-import org.apache.spark.sql.DataFrameReader;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+
 
 public class SessionRDD {
 	public static void main(String[]args) {
 		final SparkSession sparkSession = SparkSession.builder().appName("WAZZUF").master("local[6]").getOrCreate();
 		final DataFrameReader dataFrameReader = sparkSession.read().option("header", true);
-		Dataset<Row> wazzufData = dataFrameReader.csv("C:\\ITI\\Java\\FINAL PROJECT\\Wuzzuf_Jobs.csv");
+		Dataset<Row> wazzufData = dataFrameReader.csv("src\\main\\resources\\Wuzzuf_Jobs.csv");
 		wazzufData.printSchema();
 	}
 
