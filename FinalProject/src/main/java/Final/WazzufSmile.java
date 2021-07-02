294b6d44e85d86ee6a86c1c81854410d3a204011
EsmaelEzz74 <85246622+EsmaelEzz74@users.noreply.github.com>
2021-07-02 22:01:06 +0200

    Update WazzufSmile.java

--- FinalProject/src/main/java/Final/WazzufSmile.java
+++ FinalProject/src/main/java/Final/WazzufSmile.java
@@ -1,49 +1,32 @@
 package Final;
 
-import org.apache.spark.api.java.JavaRDD;
 import smile.data.DataFrame;
-import tech.tablesaw.api.Table;
 
-import javax.annotation.processing.SupportedSourceVersion;
+
+
 import java.io.IOException;
-import java.util.List;
+
 
 public class WazzufSmile {
 
-
-/*	public static void main (String[] args){
-		Utilities util = new Utilities();
-		SmileUtils smileUtils = new SmileUtils();
-		try {
-			// Load the dataset
-			Table wazzufData =smileUtils.loadDataFromCVS("C:\\ITI\\Java\\FINAL PROJECT\\Wuzzuf_Jobs.csv");
-			// Print the data structure
-			String structure =smileUtils.getDataInfoStructure(wazzufData);
-			System.out.println(structure);
-			System.in.read ();
-			// Print the summary
-			String summary = smileUtils.getDataSummary(wazzufData);
-			System.out.println(summary);
-			System.in.read ();
-			// Remove duplicates
-			wazzufData.stream().distinct();
-			System.out.println(wazzufData.stream().count());
-		}catch (Exception e ){
-			e.printStackTrace();
-		}
-	}
-}
-*/
-public static void main (String[]args) throws IOException {
-	SmileUtils smileUtil = new SmileUtils();
-	/* load wazzuf dataset */
-	DataFrame wazzufData =smileUtil.readCSV("C:\\ITI\\Java\\FINAL PROJECT\\Wuzzuf_Jobs.csv");
-	/* print the dataset summary */
-	smileUtil.getDataSummery(wazzufData);
-	/* print the dataset structure */
-	smileUtil.getDataStructure(wazzufData);
-	/* omit null rows */
-	smileUtil.processNullData(wazzufData);
+	public static void main (String[]args) throws IOException {
+		SmileUtils smileUtil = new SmileUtils();
+		/* load wazzuf dataset */
+		DataFrame wazzufData =smileUtil.readCSV("src/main/resources/Wuzzuf_Jobs.csv");
+		/* print the dataset summary */
+		System.out.println("The Dataset Summary");
+		System.out.println("------------------------------------------------------------");
+		smileUtil.getDataSummery(wazzufData);
+		System.in.read();
+		/* print the dataset structure */
+		System.out.println("The Dataset Structure");
+		System.out.println("------------------------------------------------------------");
+		smileUtil.getDataStructure(wazzufData);
+		System.in.read();
+		/* omit null rows */
+		System.out.println("Clean Null Rows");
+		System.out.println("------------------------------------------------------------");
+		smileUtil.processNullData(wazzufData);
 
 
 
